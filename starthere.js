7722e74622d8640466d6cb3147d4914c688046c7
celestinopena-nbcuni <celestino.pena@nbcuni.com>
2020-01-28 12:00:52 -0500

    Add config file reading

--- starthere.js
+++ starthere.js
@@ -1,4 +1,8 @@
-var AWS = require("aws-sdk");
+const AWS = require("aws-sdk"),
+  path = require('path'),
+  fs = require('fs'),
+  YAML = require('yaml')
+
 
 AWS.config.getCredentials(function(err) {
   if (err) console.log(err.stack);
@@ -7,6 +11,50 @@ AWS.config.getCredentials(function(err) {
     console.log("Access key:", AWS.config.credentials.accessKeyId);
     console.log("Secret access key:", AWS.config.credentials.secretAccessKey);
     AWS.config.update({region: 'us-east-1'});
-    console.log('Region', AWS.config.region);
+    console.log('Region', AWS.config.region, JSON.stringify({
+      dir: __dirname,
+      filespec: path.join(__dirname, '/settings.yaml'),
+      filetxt: readFile('./settings.yaml')
+    }, null, 2));
+    console.log('Config', readConfig('./settings.yaml'));
   }
 });
+
+function readFile(filepath) {
+  let contents = null;
+  try {
+    contents = fs.readFileSync(filepath, 'utf8')
+  } catch (err) { console.error(err) }
+  return contents
+}
+
+function readConfig(filepath) {
+  let contents = null;
+  try {
+    contents = YAML.parse(fs.readFileSync(filepath, 'utf8'))
+  } catch (err) { console.error(err) }
+  return contents
+}
+
+function showObj(obj) { JSON.stringify(obj, null, 2) }
+
+/*
+function hitDB(params) {
+  // Initialize parameters needed to call DynamoDB
+  var slotParams = {
+    Key : {'slotPosition' : {N: '0'}},
+    TableName : 'slotWheels',
+    ProjectionExpression: 'imageFile'
+  };
+
+  // prepare request object for call to DynamoDB
+  var request = new AWS.DynamoDB({region: 'us-west-2', apiVersion: '2012-08-10'}).getItem(slotParams);
+  // log the name of the image file to load in the slot machine
+  request.on('success', function(response) {
+    // logs a value like "cherries.jpg" returned from DynamoDB
+    console.log(response.data.Item.imageFile.S);
+  });
+  // submit DynamoDB request
+  request.send();
+}
+*/
