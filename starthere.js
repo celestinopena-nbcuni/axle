fcf2e67a6812bce94bed68f0eb972cf281dc14d7
celestinopena-nbcuni <celestino.pena@nbcuni.com>
2020-01-28 15:25:18 -0500

    Create a local db table

--- starthere.js
+++ starthere.js
@@ -1,24 +1,33 @@
-const AWS = require("aws-sdk"),
+const AWS = require('aws-sdk'),
   path = require('path'),
   fs = require('fs'),
   YAML = require('yaml')
 
+// showCredentials()
+const cmdlineParams = arg(1)
+if (!cmdlineParams) {
+  console.log('usage: node starthere.js [C | Q]');
+} else {
+  hitDB(cmdlineParams)
+}
 
-AWS.config.getCredentials(function(err) {
-  if (err) console.log(err.stack);
-  // credentials not loaded
-  else {
-    console.log("Access key:", AWS.config.credentials.accessKeyId);
-    console.log("Secret access key:", AWS.config.credentials.secretAccessKey);
-    AWS.config.update({region: 'us-east-1'});
-    console.log('Region', AWS.config.region, JSON.stringify({
-      dir: __dirname,
-      filespec: path.join(__dirname, '/settings.yaml'),
-      filetxt: readFile('./settings.yaml')
-    }, null, 2));
-    console.log('Config', readConfig('./settings.yaml'));
-  }
-});
+function showCredentials() {
+  AWS.config.getCredentials(function(err) {
+    if (err) console.log(err.stack);
+    // credentials not loaded
+    else {
+      console.log('Access key:', AWS.config.credentials.accessKeyId);
+      console.log('Secret access key:', AWS.config.credentials.secretAccessKey);
+      AWS.config.update({region: 'us-east-1'});
+      console.log('Region', AWS.config.region, obj2str({
+        dir: __dirname,
+        filespec: path.join(__dirname, '/settings.yaml'),
+        filetxt: readFile('./settings.yaml')
+      }))
+      console.log('Config', readConfig('./settings.yaml'))
+    }
+  })
+}
 
 function readFile(filepath) {
   let contents = null;
@@ -36,25 +45,182 @@ function readConfig(filepath) {
   return contents
 }
 
-function showObj(obj) { JSON.stringify(obj, null, 2) }
-
-/*
-function hitDB(params) {
-  // Initialize parameters needed to call DynamoDB
-  var slotParams = {
-    Key : {'slotPosition' : {N: '0'}},
-    TableName : 'slotWheels',
-    ProjectionExpression: 'imageFile'
-  };
-
-  // prepare request object for call to DynamoDB
-  var request = new AWS.DynamoDB({region: 'us-west-2', apiVersion: '2012-08-10'}).getItem(slotParams);
-  // log the name of the image file to load in the slot machine
-  request.on('success', function(response) {
-    // logs a value like "cherries.jpg" returned from DynamoDB
-    console.log(response.data.Item.imageFile.S);
-  });
-  // submit DynamoDB request
-  request.send();
+function obj2str(obj) { return JSON.stringify(obj, null, 2) }
+function arg(i) {
+  if (i<0 || i>=process.argv.length-1) return '';
+  return process.argv[i+1];
 }
-*/
+
+function hitDB(cmd = 'Q') {
+  AWS.config.getCredentials(function(err) {
+    if (err) { console.log(err.stack);  } // credentials not loaded
+    else {
+      AWS.config.update({
+        region: 'us-east-1',
+        endpoint: 'http://localhost:8000'
+      });
+      if (cmd==='C') {
+        // createTable()
+        createTelemundoTable()
+      } else {
+        queryDB()
+      }
+    }
+  })
+}
+
+function createTable() {
+  console.log('Create table', new Date());
+  const dynamodb = new AWS.DynamoDB();
+  const params = {
+    TableName : 'Movies',
+    KeySchema: [
+      { AttributeName: 'year', KeyType: 'HASH'},  //Partition key
+      { AttributeName: 'title', KeyType: 'RANGE' }  //Sort key
+    ],
+    AttributeDefinitions: [
+      { AttributeName: 'year', AttributeType: 'N' },
+      { AttributeName: 'title', AttributeType: 'S' }
+    ],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 10,
+      WriteCapacityUnits: 10
+    }
+  }
+  dynamodb.createTable(params, function(err, data) {
+    if (err) {
+      console.error('Unable to create table. Error JSON:', JSON.stringify(err, null, 2));
+    } else {
+      console.log('Created table. Table description JSON:', JSON.stringify(data, null, 2));
+    }
+  })
+}
+
+function createTelemundoTable() {
+  const tableParams = {
+    'TableName': 'TelemundoContentparams',
+    'KeyAttributesparams': {
+      'PartitionKeyparams': {
+        'AttributeNameparams': 'contentId',
+        'AttributeType': 'S'
+      }
+    },
+    'NonKeyAttributes': [
+      {
+        'AttributeName': 'itemType',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'title',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'datePublished',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'source',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'authorPhotoLink',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'byline',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'body',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'coverImage',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'coverImageDescription',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'mpxVideo',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'mpxVideoReference',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'videoCredit',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'featuredGallery',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'promoTitle',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'promoImage',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'promoKicker',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'promoDescription',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'showSeason',
+        'AttributeType': 'S'
+      },
+      {
+        'AttributeName': 'brand',
+        'AttributeType': 'SS'
+      },
+      {
+        'AttributeName': 'category',
+        'AttributeType': 'SS'
+      },
+      {
+        'AttributeName': 'keywords',
+        'AttributeType': 'SS'
+      },
+      {
+        'AttributeName': 'people',
+        'AttributeType': 'SS'
+      },
+      {
+        'AttributeName': 'roleProfile',
+        'AttributeType': 'SS'
+      },
+      {
+        'AttributeName': 'status',
+        'AttributeType': 'S'
+      }
+    ]
+  }
+  console.log('Table params', obj2str(tableParams));
+}
+
+function queryDB() {
+  const docClient = new AWS.DynamoDB.DocumentClient()
+  // Initialize parameters needed to call DynamoDB
+  const params = {
+    TableName: 'tlmd-crud-dynamodb-taximg-dqstore',
+    Key: {
+      'dqid': '8155'
+    } // ,  ProjectionExpression: 'filter'
+  }
+  docClient.get(params, function(err, data) {
+    if (err) {
+      console.error('Unable to read item. Error JSON:', JSON.stringify(err, null, 2));
+    } else {
+      console.log('GetItem succeeded:', JSON.stringify(data, null, 2));
+    }
+  })
+}
